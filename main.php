b7df671ec77614855ca2e007c6bf2409e682029f
Nestovich <nestxproduction@gmail.com>
2024-10-23 10:18:49 +0200

    Add files via upload

--- main.php
+++ main.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,48 +9,61 @@
 <body>
     
 
-<DIV id="top">
- <div id="naglowek"><img src="" alt="" width="75"></div>
- <div id="linki">
- <ul>
-      
-	 <li><a href="#">AAAAA</a></li>
-     <li><a href="#">BBBBB</a></li>
-	 <li><a href="#">CCCCC</a></li>
-	 <li><a href="#">DDDDD</a></li>
-     <li><a href="#">EEEEE</a></li>
-	 <li><a href="#">FFFFF</a></li>
-	 
-</ul>
+    <DIV id="top">
+        <div id="naglowek"></div>
+        <div id="linki">
+            <ul>
+                
+                <li><a href="glowna.php">GŁÓWNA</a></li>
+                <li><a href="#">Miasta</a></li>
+                <li><a href="#">TWÓRCY</a></li>
+                
+                
+            </ul>
+        </div>
+    </div>
 
+    <?php
+        require("config.php");
+        $miasta = "SELECT * FROM miasta";
+        $result = $conn->query($miasta);
+
+        while($wynik = mysqli_fetch_assoc($result)) {
+            echo "<a>" . $wynik['nazwa'] . "</a>";
+            echo "<img src=" . $wynik['zdjecie'] . ".jpg">;
+        }
+
+    ?>
+
+
+
+    <script>
+        const snowflakesCount = 100;
+
+    for (let i = 0; i < snowflakesCount; i++) {
+        createSnowflake();
+    }
+
+    function createSnowflake() {
+        const snowflake = document.createElement('div');
+        snowflake.className = 'snowflake';
+        snowflake.innerHTML = '.'; 
+
+    
+        snowflake.style.left = Math.random() * window.innerWidth + 'px';
+        snowflake.style.fontSize = Math.random() * 10 + 10 + 'px'; 
+        snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; 
+        snowflake.style.opacity = Math.random(); 
+
+        document.body.appendChild(snowflake);
+
+        
+        snowflake.addEventListener('animationend', () => {
+            snowflake.remove();
+            createSnowflake(); 
+        });
+    }
+    </script>
 
 </body>
 </html>
-
-<script>
-    const snowflakesCount = 100;
-
-for (let i = 0; i < snowflakesCount; i++) {
-    createSnowflake();
-}
-
-function createSnowflake() {
-    const snowflake = document.createElement('div');
-    snowflake.className = 'snowflake';
-    snowflake.innerHTML = '.'; 
-
-   
-    snowflake.style.left = Math.random() * window.innerWidth + 'px';
-    snowflake.style.fontSize = Math.random() * 10 + 10 + 'px'; 
-    snowflake.style.animationDuration = Math.random() * 3 + 2 + 's'; 
-    snowflake.style.opacity = Math.random(); 
-
-    document.body.appendChild(snowflake);
-
-    
-    snowflake.addEventListener('animationend', () => {
-        snowflake.remove();
-        createSnowflake(); 
-    });
-}
-</script>
